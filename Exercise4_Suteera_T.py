8edfb23abbfa3581f31024d21489fc4148a022d4
SuteeraThid <suteera_pay@hotmail.co.th>
2021-07-21 16:58:22 +0700

    Exercise4_1

---
+++ Exercise4_Suteera_T.py
@@ -0,0 +1,10 @@
+FoundationEnglish = 83
+GeneralBusiness = 70.48
+IntroductionToComputerSystems = 51.3
+ComputerProgramming = 63.45
+
+print("---Your Score---")
+print("Foundation English               :", FoundationEnglish, "คะแนน")
+print("General Business                 :", GeneralBusiness, "คะแนน")
+print("Introduction to Computer Systems :", IntroductionToComputerSystems, "คะแนน")
+print("Computer Programming             :", ComputerProgramming, "คะแนน")
